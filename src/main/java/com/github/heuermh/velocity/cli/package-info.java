27e1193e0744ac4c7caa29e64d8c2c251525f323
Michael Heuer <heuermh@acm.org>
2014-09-05 20:51:03 -0500

    fix license text

--- src/main/java/com/github/heuermh/velocity/cli/package-info.java
+++ src/main/java/com/github/heuermh/velocity/cli/package-info.java
@@ -1,5 +1,5 @@
 /*
- * The authors of this file licenses it to you under the
+ * The authors of this file license it to you under the
  * Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You
  * may obtain a copy of the License at
